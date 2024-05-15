37400c64caee2369bf9aefb0b4a84fab423965f9
amay077 <okuokuoku@gmail.com>
2024-05-15 22:23:06 +0900

    up

--- netlify/functions/ver.js
+++ netlify/functions/ver.js
@@ -1,7 +1,7 @@
 const handler = async (event) => {
   return {
     statusCode: 200,
-    body: JSON.stringify({ build_at: '2024-05-15T09:44:43.275Z', env_ver: process.env.ENV_VER })
+    body: JSON.stringify({ build_at: 'xxxx', env_ver: process.env.ENV_VER })
   }
 }
 
