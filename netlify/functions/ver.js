a5929b616b4a79c21976bdd4fa3201cd9923cd0e
amay077 <okuokuoku@gmail.com>
2024-05-19 11:15:01 +0900

    ver

--- netlify/functions/ver.js
+++ netlify/functions/ver.js
@@ -1,7 +1,15 @@
+// Need for CORS
+const resHeaders = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Credentials': true,
+  'Content-Type': 'application/json',
+};
+
 const handler = async (event) => {
   return {
     statusCode: 200,
-    body: JSON.stringify({ build_at: 'xxxxxx', env_ver: process.env.ENV_VER })
+    headers: resHeaders,
+    body: JSON.stringify({ build_at: '{{built_at}}', env_ver: process.env.ENV_VER })
   }
 }
 
